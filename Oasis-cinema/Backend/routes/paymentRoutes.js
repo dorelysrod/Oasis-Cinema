a20d475a95597fdf87f3af155bf4f0ca5a57162a
Dorelys Rodriguez <rodriguezdorelys9@gmail.com>
2025-02-22 00:38:21 +0100

    initial commit

--- Oasis-cinema/Backend/routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createCheckoutSession } from "../controllers/paymentController.js";
-
-const router = express.Router();
-
-router.post("/checkout", createCheckoutSession);
-
-export default router;
--- Oasis-cinema/src/context/BookingContext.jsx
+++ Oasis-cinema/src/context/BookingContext.jsx
@@ -57,3 +57,4 @@ export const BookingProvider = ({ children }) => {
 };
 
 export default BookingContext;
+
